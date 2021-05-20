41c44113d91472080a2d7ebb3e160be37953fcb0
Vihviwk <ottavwk@gmail.com>
2021-05-20 18:38:35 +0200

    Aggiunto l'adapter per lo spinner e creato i bottoni sulla main activity. I bottoni sono ancora di misura arbitraria
    Signed-off-by: Vihviwk <ottavwk@gmail.com>

--- app/src/main/java/it/uninsubria/moneybook/ui/OverviewFragment.kt
+++ app/src/main/java/it/uninsubria/moneybook/ui/OverviewFragment.kt
@@ -1,20 +1,58 @@
 package it.uninsubria.moneybook.ui
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
 import it.uninsubria.moneybook.R
+import kotlinx.android.synthetic.main.fragment_overview.*
 
-class OverviewFragment : Fragment() {
+
+class OverviewFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_overview, container, false)
+
+        val spinner = view.findViewById<Spinner>(R.id.period_spinner)
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.current_period,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = this
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        return view
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+        //make toast
+        val item : String = parent!!.getItemAtPosition(position).toString()
+        //showing selected spinner item
+        Toast.makeText(parent.context, "Selected $item", Toast.LENGTH_LONG).show()
+
+        TODO("cambiare l'amount in base al periodo")
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
 
 }
