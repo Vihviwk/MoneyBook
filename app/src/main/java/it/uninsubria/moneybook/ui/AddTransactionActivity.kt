dba745a75ac511c9c760bc2c699630b237651e17
Vihviwk <ottavwk@gmail.com>
2021-05-22 17:51:44 +0200

    Creato il layout della seconda activity
    Signed-off-by: Vihviwk <ottavwk@gmail.com>

--- app/src/main/java/it/uninsubria/moneybook/ui/AddTransactionActivity.kt
+++ app/src/main/java/it/uninsubria/moneybook/ui/AddTransactionActivity.kt
@@ -1,16 +1,33 @@
 package it.uninsubria.moneybook.ui
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import it.uninsubria.moneybook.R
+import kotlinx.android.synthetic.main.add_transaction_activity.*
 
 
 private const val TAG = "Add Transaction"
 
-class AddTransactionActivity: AppCompatActivity() {
+class AddTransactionActivity: AppCompatActivity(), View.OnClickListener {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.v(TAG, "onCreate")
+        setContentView(R.layout.add_transaction_activity)
+
+        okButton.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        //TODO("Not yet implemented")
+        //store the transaction in the db or handle invalid input
+    }
+
+
+
 }
