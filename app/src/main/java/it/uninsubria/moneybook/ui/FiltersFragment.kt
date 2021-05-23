2834de13d7d3b53ca2c1a745d5c8d04e2e77329e
Vihviwk <ottavwk@gmail.com>
2021-05-23 18:15:57 +0200

    piccoli cambiamenti
    Signed-off-by: Vihviwk <ottavwk@gmail.com>

---
+++ app/src/main/java/it/uninsubria/moneybook/ui/FiltersFragment.kt
@@ -0,0 +1,22 @@
+package it.uninsubria.moneybook.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import it.uninsubria.moneybook.R
+
+
+class FiltersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_filters, container, false)
+    }
+
+}
